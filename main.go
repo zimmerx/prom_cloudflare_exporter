9f4d289fae51723b7985b66a18645d384dd4b46f
zimmerx <vakh87@gmail.com>
2023-08-25 02:23:44 +0000

    Initial

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
-function main.go
+package main
 
-include {
-	
-}
+import (
+	"regexp"
+)
+
+var exp = regexp.MustCompile(`^(?P<remote>[^ ]*) (?P<host>[^ ]*) (?P<user>[^ ]*) \[(?P<time>[^\]]*)\] \"(?P<method>\w+)(?:\s+(?P<path>[^\"]*?)(?:\s+\S*)?)?\" (?P<status_code>[^ ]*) (?P<size>[^ ]*)(?:\s"(?P<referer>[^\"]*)") "(?P<agent>[^\"]*)" (?P<urt>[^ ]*)$`)
